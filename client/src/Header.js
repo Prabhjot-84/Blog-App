cbbd3142e5b29f303cb26d2d1f1ba50dd913138e
Prabhjot-84 <prabhjotsingh1645@gmail.com>
2024-11-17 23:22:47 +0530

    resolving deployment error

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,58 +4,54 @@ import { UserContext } from './userContext';
 import Icon from './icon.png';
 
 const Header = () => {
-  
-  const {setUserInfo, userInfo} = useContext(UserContext);
+  const { setUserInfo, userInfo } = useContext(UserContext);
 
-  // sending http request for jwt verfication
+  // sending http request for jwt verification
   useEffect(() => {
-    fetch( 'http://localhost:4000/profile', {
+    fetch('http://localhost:4000/profile', {
       credentials: 'include',
     })
-    .then( response => {
-      response.json().then( userInfo => {
-        setUserInfo(userInfo);
+      .then((response) => response.json())
+      .then((data) => {
+        setUserInfo(data);
       })
-    })
-  }, []);
-
+      .catch((error) => console.error('Error fetching user profile:', error));
+  }, [setUserInfo]);
 
   // Function to Log-out
   function logout() {
-    fetch( 'http://localhost:4000/logout', {
+    fetch('http://localhost:4000/logout', {
       credentials: 'include',
       method: 'POST',
     })
-    setUserInfo(null);
+      .then(() => setUserInfo(null))
+      .catch((error) => console.error('Error during logout:', error));
   }
-  
+
   const username = userInfo?.username;
 
-
   return (
-    <header> 
-        <div className='header-logo'>
-          <img src={Icon} alt='' />
-          <Link to="/" className="logo"> &nbsp; Blogtown </Link>
-        </div>
-        <nav>
-
-          {username && (
-            <>
-              <button className='logout' onClick={logout} > Logout <img className='logout_img' src='./logout_img.png' alt='' /> </button>
-            </>
-          )}
-
-          { !username && (
-            <>
-              <Link to="/login"> Login </Link>
-              <Link to="/register"> Register </Link>
-            </>
-          )}
-          
-        </nav>
+    <header>
+      <div className="header-logo">
+        <img src={Icon} alt="" />
+        <Link to="/" className="logo">
+          &nbsp; Blogtown
+        </Link>
+      </div>
+      <nav>
+        {username ? (
+          <button className="logout" onClick={logout}>
+            Logout <img className="logout_img" src="./logout_img.png" alt="" />
+          </button>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/Headline.js
+++ client/src/Headline.js
@@ -1,33 +1,30 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { UserContext } from './userContext';
 
 const Headline = () => {
-
-  const {setUserInfo, userInfo} = useContext(UserContext);
+  const { userInfo } = useContext(UserContext);
 
   const username = userInfo?.username;
 
   return (
-    <div className='headlineCont'>
-        <div className='headlineTxt'>
-            <h1 className='heading1'>Discover, Share, and Explore</h1>
-            <h3 className='heading2'>Your Ultimate Blogging Destination</h3>
-            <i className='heading3'>"Unleash your creativity and join our vibrant community of writers and readers. Dive into a world of captivating stories and insightful articles."</i>
-            {/* <button className='createBtn'> Create </button> */}
+    <div className="headlineCont">
+      <div className="headlineTxt">
+        <h1 className="heading1">Discover, Share, and Explore</h1>
+        <h3 className="heading2">Your Ultimate Blogging Destination</h3>
+        <i className="heading3">
+          "Unleash your creativity and join our vibrant community of writers and readers. Dive into a world of captivating stories and insightful articles."
+        </i>
 
-            {username && (
-              <>
-                <Link to="/create" className='createBtn'> 
-                  Create 
-                </Link>
-              </>
-            )}
-
-        </div> 
-        <div className='exploreImg'></div> 
+        {username && (
+          <Link to="/create" className="createBtn">
+            Create
+          </Link>
+        )}
+      </div>
+      <div className="exploreImg"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Headline
+export default Headline;
